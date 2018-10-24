1b6713f9296879e3201a16a53dc7e347f844edc4
Deran Manuel <deran.surdez@students.fhnw.ch>
2018-10-24 14:19:34 +0200

    New Intranet

---
+++ Website/Intranet.php
@@ -0,0 +1,25 @@
+
+
+<html>
+<head>
+		<link rel = "stylesheet" href = "Intranet.css">
+	</head>
+        
+        <body>
+            
+            <header class="navigation">
+			<p>
+				<a href = "../Website/Index.php">Home</a>
+			</p>
+		</header>
+            <h1>
+                Kurs hinzufügen
+            </h1>
+            
+        </body>
+<?php
+
+echo "Sie haben noch keine Kurse hinzugefügt!";
+?>
+
+</html>
