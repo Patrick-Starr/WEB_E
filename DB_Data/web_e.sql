dc9a67ebdb315d856250d6a197e29f459c28371e
pätte <ptte@DESKTOP-9I1PMTR.edu.ds.fhnw.ch>
2018-10-31 15:13:30 +0100

    New DataBase

--- DB_Data/web_e.sql
+++ DB_Data/web_e.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 31. Okt 2018 um 13:26
+-- Erstellungszeit: 31. Okt 2018 um 15:12
 -- Server-Version: 10.1.35-MariaDB
 -- PHP-Version: 7.2.9
 
@@ -19,21 +19,27 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Datenbank: `logins`
+-- Datenbank: `web_e`
 --
-CREATE DATABASE IF NOT EXISTS `logins` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_520_ci;
-USE `logins`;
+CREATE DATABASE IF NOT EXISTS `web_e` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_520_ci;
+USE `web_e`;
 
 -- --------------------------------------------------------
 
 --
--- Tabellenstruktur für Tabelle `schools`
+-- Tabellenstruktur für Tabelle `courses`
 --
 
-CREATE TABLE `schools` (
-  `id` int(5) NOT NULL,
-  `name` varchar(30) COLLATE utf8_unicode_520_ci NOT NULL,
-  `place` varchar(30) COLLATE utf8_unicode_520_ci NOT NULL
+CREATE TABLE `courses` (
+  `CID` int(5) NOT NULL,
+  `UID` int(5) NOT NULL,
+  `Course` varchar(120) COLLATE utf8_unicode_520_ci NOT NULL,
+  `Link` varchar(120) COLLATE utf8_unicode_520_ci NOT NULL,
+  `Duration` int(5) NOT NULL,
+  `Start` date NOT NULL,
+  `Form` varchar(8) COLLATE utf8_unicode_520_ci NOT NULL,
+  `Place` varchar(50) COLLATE utf8_unicode_520_ci NOT NULL,
+  `Created` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
 
 -- --------------------------------------------------------
@@ -43,52 +49,44 @@ CREATE TABLE `schools` (
 --
 
 CREATE TABLE `users` (
-  `user_id` int(8) NOT NULL,
-  `user_name` varchar(20) COLLATE utf8_unicode_520_ci NOT NULL,
-  `user_prename` varchar(20) COLLATE utf8_unicode_520_ci NOT NULL,
-  `email` varchar(50) COLLATE utf8_unicode_520_ci NOT NULL,
-  `authorization` varchar(5) COLLATE utf8_unicode_520_ci NOT NULL,
-  `password` varchar(20) COLLATE utf8_unicode_520_ci NOT NULL
+  `UID` int(5) NOT NULL,
+  `School` varchar(80) COLLATE utf8_unicode_520_ci NOT NULL,
+  `Place` varchar(50) COLLATE utf8_unicode_520_ci NOT NULL,
+  `E-Mail` varchar(50) COLLATE utf8_unicode_520_ci NOT NULL,
+  `Password` varchar(20) COLLATE utf8_unicode_520_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_520_ci;
-
---
--- Daten für Tabelle `users`
---
-
-INSERT INTO `users` (`user_id`, `user_name`, `user_prename`, `email`, `authorization`, `password`) VALUES
-(1, 'Müller', 'Peter', 'pm@eatmyshorts.ch', 'guest', '1');
 
 --
 -- Indizes der exportierten Tabellen
 --
 
 --
--- Indizes für die Tabelle `schools`
+-- Indizes für die Tabelle `courses`
 --
-ALTER TABLE `schools`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`CID`);
 
 --
 -- Indizes für die Tabelle `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_id`);
+  ADD PRIMARY KEY (`UID`);
 
 --
 -- AUTO_INCREMENT für exportierte Tabellen
 --
 
 --
--- AUTO_INCREMENT für Tabelle `schools`
+-- AUTO_INCREMENT für Tabelle `courses`
 --
-ALTER TABLE `schools`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `courses`
+  MODIFY `CID` int(5) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT für Tabelle `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `UID` int(5) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
