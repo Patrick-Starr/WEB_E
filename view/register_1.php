6f28997cd6b694cf40e0ec7d9548814fb7313521
Deran Manuel <Deran Manuel@DeranSurdez.edu.ds.fhnw.ch>
2018-11-07 18:29:14 +0100

    new Order and new files

--- view/register_1.php
+++ view/register_1.php
@@ -8,23 +8,23 @@
 		<h1>Registrierung</h1>
 		<link rel = "stylesheet" href = "../CSS_Files/register.css">
 	</head>
-<!--agajgaj-->
-          
+	
+	<body>
 		<div id="table">
             <form action="index.php">
 			
 			<table class="info">
 				<tr> 
-        			<td height="45">Place:</td>
-        			<td height="45"><input type="text" name="place" size="50" /></td>
+        			<td height="45">E-mail Adresse:</td>
+        			<td height="45"><input type="email" name="email" size="50" /></td>
 				</tr>
 				<tr> 
-        			<td height="45">E-mail:</td>
-                                <td height="45"><input type="email" name="email" size="50" /></td>
-				</tr>
-				<tr> 
-        			<td height="45">Passwort :</td>
+        			<td height="45">Passwort:</td>
         			<td height="45"><input type="password" name="password" size="50" /></td>
+				</tr>
+				<tr> 
+        			<td height="45">Passwort wiederholen:</td>
+        			<td height="45"><input type="password" name="passwordAgain" size="50" /></td>
 				</tr>
 			</table> </br> </br>
 	
@@ -34,16 +34,15 @@
 			
 			</form>	
 		</div>
-   
             
-            
-<!--       <script type="text/javascript">
+            <script type="text/javascript">
             function funktion(){
             	$.ajax({url:"sendMail.php"})
                 alert("Ihre Daten werden verarbeitet, Sie werden per E-Mail informiert falls die Registrierung erfolgreich war. Dies kann einige Tage dauern."); 
             }
-         </script> -->
+            </script> 
             
             
 	</body>
 	
+</html>
