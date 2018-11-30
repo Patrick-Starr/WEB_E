a5c9baa56e068db09ba2285c8007b6de71a1dd61
Startklar <Startklar@LAPTOP-LP1HF0KU.edu.ds.fhnw.ch>
2018-11-30 15:55:22 +0100

    Index

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <!--  Initialize all needed, Homepage is shown on home.php  -->
 
 <?php
-header("location: index.php");
-header("location: view/home.php");
+header("location: ../view/home.php");
 
 
 
