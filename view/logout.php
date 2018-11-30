509d953010fe6bd0e504eb100c8129bc0a68e11d
Startklar <Startklar@LAPTOP-LP1HF0KU.edu.ds.fhnw.ch>
2018-11-30 13:47:45 +0100

    Div

--- view/logout.php
+++ view/logout.php
@@ -5,8 +5,8 @@ include_once '../DAO/DB_Connection.php';
 if (isset($_SESSION['user']))
 {
     destroySession();
-    echo "<div class='main'>You have been logged out. Please " .
-        "<a href='home.php'>click here</a> to refresh the screen.";
+    header("location:home.php");
+
 }
 
 
