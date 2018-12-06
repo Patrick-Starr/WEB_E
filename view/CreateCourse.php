0bdb62ab5d13157137ff48db71d479059186532c
pätte <ptte@DESKTOP-9I1PMTR.edu.ds.fhnw.ch>
2018-12-06 17:50:05 +0100

    edit and delete works

--- view/CreateCourse.php
+++ view/CreateCourse.php
@@ -4,8 +4,15 @@ include '../header.php';
 include '../DAO/CoursesDAO.php';
 
 $CID = $_GET['wert'];
+$click = $_GET['click'];
 
+if ($click === "delete") {
+    Courses::delete($CID);
+}
+
+if ($click === "safe") {
     Courses::update($CID, $_POST['course'], $_POST['url'], $_POST['duration'], $_POST['run'], $_POST['art'], $_POST['place']);
+}
 
-header("location:Intranet.php");
+    header("location:Intranet.php");
 ?>
