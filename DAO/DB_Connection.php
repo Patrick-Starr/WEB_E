5c142bae3ab712ad1a0c91de9719b81c163f0780
pätte <ptte@192.168.1.120>
2018-12-07 16:50:07 +0100

    DB Connect

--- DAO/DB_Connection.php
+++ DAO/DB_Connection.php
@@ -14,10 +14,10 @@ class Database
 
     public static function connect()
     {
-        $dbName = Config::get('database.name');
-        $dbHost = Config::get('database.host');
-        $dbUsername = Config::get('database.user');
-        $dbUserPassword = Config::get('database.password');
+        $dbName = "jq7vszrcc65rhjnw" //Config::get('database.name');
+        $dbHost = "sabaik6fx8he7pua.chr7pe7iynqr.eu-west-1.rds.amazonaws.com" //Config::get('database.host');
+        $dbUsername = "ivm1bdn2bi4uux6o" //Config::get('database.user');
+        $dbUserPassword = "mlxumziv1rkqz7ix" //Config::get('database.password');
         // One connection through whole application
         if (null == self::$cont) {
             try {
