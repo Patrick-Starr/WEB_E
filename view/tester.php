5190565a5916e110b155c6e4724e27dd21792bab
pätte <ptte@DESKTOP-9I1PMTR.edu.ds.fhnw.ch>
2018-12-10 17:18:53 +0100

    testing  lostPW

--- view/tester.php
+++ view/tester.php
@@ -1,6 +1,5 @@
 <?php 
 
-include_once '../DAO/EmailServiceClient.php';
 include_once '../DAO/userDAO.php';
 
 $mailFromDB = userDAO::getAllMails();
@@ -8,10 +7,15 @@ $mailArray = explode("?", $mailFromDB);
 $in = $in_array($mail, $mailArray);
 $in_array($mail, $mailArray);
 
-var_dump($mailFromDB);
-var_dump($mailArray);
-var_dump($in_array);
-var_dump($in);
+$a = var_dump($mailFromDB);
+$b = var_dump($mailArray);
+$c = var_dump($in_array);
+$d = var_dump($in);
+
+echo $a;
+echo $b;
+echo $c;
+echo $d;
 
 
 ?>
