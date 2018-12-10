b6efc705567b39df41135ef8a7d5d8753688bf22
pätte <ptte@DESKTOP-9I1PMTR.edu.ds.fhnw.ch>
2018-12-10 16:40:43 +0100

    return to home after contact success

--- view/sendcontact.php
+++ view/sendcontact.php
@@ -4,4 +4,6 @@ include '../header.php';
 include_once '../DAO/EmailServiceClient.php';
 
 EmailServiceClient::sendEmail('deran.surdez@students.fhnw.ch', 'Contact von ' . $_POST['name'], 'Name: ' . $_POST['name'] . "<br>" . 'Subject: ' . $_POST['subject'] . "<br>" . 'Email: ' . $_POST['email'] . "<br>" . 'Anliegen: ' . $_POST['text']);
+
+header("location:home.php");
 ?>
