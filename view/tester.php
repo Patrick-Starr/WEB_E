38c88522b8ab22a2371a044d7941a1ceaca51bce
pätte <ptte@DESKTOP-9I1PMTR.edu.ds.fhnw.ch>
2018-12-10 17:31:29 +0100

    testing..

--- view/tester.php
+++ view/tester.php
@@ -3,40 +3,41 @@
 include_once '../DAO/userDAO.php';
 
 echo "1";
-$mailFromDB = userDAO::getAllMails();
-echo "2";
-$mailArray = explode("?", $mailFromDB);
-echo "3";
-$in = $in_array("admin@fhnw.ch", $mailArray);
-echo "4";
-$in_array("admin@fhnw.ch", $mailArray);
-echo "5";
-
 try {
+$mailFromDB = userDAO::getAllMails();
 $a = var_dump($mailFromDB);
+echo $a;
 } catch (Exception $e) {
     echo "a";
 }
+
+echo "2";
 try {
+$mailArray = explode("?", $mailFromDB);
 $b = var_dump($mailArray);
+echo $b;
 } catch (Exception $e) {
     echo "b";
 }
+
+echo "3";
 try {
+$in = $in_array("admin@fhnw.ch", $mailArray);
 $c = var_dump($in_array);
+echo $c;
 } catch (Exception $e) {
     echo "c";
 }
+
+echo "4";
 try {
+$in_array("admin@fhnw.ch", $mailArray);
 $d = var_dump($in);
+echo $d;
 } catch (Exception $e) {
     echo "d";
 }
 
-echo $a;
-echo $b;
-echo $c;
-echo $d;
-
+echo "end";
 
 ?>
