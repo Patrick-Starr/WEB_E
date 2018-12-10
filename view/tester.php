c034bad0f6201d41fcde3d38fcb77a38786b6d33
pätte <ptte@DESKTOP-9I1PMTR.edu.ds.fhnw.ch>
2018-12-10 17:22:22 +0100

    again testing

--- view/tester.php
+++ view/tester.php
@@ -7,10 +7,26 @@ $mailArray = explode("?", $mailFromDB);
 $in = $in_array($mail, $mailArray);
 $in_array($mail, $mailArray);
 
+try {
 $a = var_dump($mailFromDB);
+} catch (Exception $e) {
+    echo "a";
+}
+try {
 $b = var_dump($mailArray);
+} catch (Exception $e) {
+    echo "b";
+}
+try {
 $c = var_dump($in_array);
+} catch (Exception $e) {
+    echo "c";
+}
+try {
 $d = var_dump($in);
+} catch (Exception $e) {
+    echo "d";
+}
 
 echo $a;
 echo $b;
