8c7f2d29538811c2a612ac7d69a0c74b19bcc728
Snikker <32131056+Snikker@users.noreply.github.com>
2019-01-17 18:08:30 +0100

    Update sendcontact.php

--- view/sendcontact.php
+++ view/sendcontact.php
@@ -5,7 +5,7 @@ include_once '../DAO/EmailServiceClient.php';
 /*
  * Sends an Email to sojo.nagaroor@students.fhnw.ch (Admin)
  */
-EmailServiceClient::sendEmail('sojo.nagaroor@students.fhnw.ch', 'Contact von ' . $_POST['name'], 'Name: ' . $_POST['name'] . "<br>" . 'Subject: ' . $_POST['subject'] . "<br>" . 'Email: ' . $_POST['email'] . "<br>" . 'Anliegen: ' . $_POST['text']);
+EmailServiceClient::sendEmail('patrick.zioerjen@students.fhnw.ch', 'Contact von ' . $_POST['name'], 'Name: ' . $_POST['name'] . "<br>" . 'Subject: ' . $_POST['subject'] . "<br>" . 'Email: ' . $_POST['email'] . "<br>" . 'Anliegen: ' . $_POST['text']);
 
 header("location:home.php");
 ?>
